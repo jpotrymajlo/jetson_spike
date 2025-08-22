75e185068c3bfa70fd04b9f655d0e8563d2d23bd
Jacek Potrymajlo <j.potrymajlo@gmail.com>
2025-08-22 20:42:00 +0200

    Added SimpleBLE

---
+++ recipes/simpleble/conanfile.py
@@ -0,0 +1,40 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+from conan.tools.scm import Git
+import os
+
+class SimpleBLEConan(ConanFile):
+    name = "simpleble"
+    version = "0.10.3"
+    license = "MIT"
+    url = "https://github.com/simpleble/simpleble"
+    description = "Cross-platform Bluetooth Low Energy library for C++."
+    topics = ("bluetooth", "ble", "cross-platform")
+    settings = "os", "compiler", "build_type", "arch"
+#    generators = "CMakeDeps", "CMakeToolchain"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def source(self):
+        git = Git(self)
+        git.clone("https://github.com/simpleble/simpleble.git", target=".", args=["--depth=1", "--branch", "v0.10.3"])
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+        deps= CMakeDeps(self)
+        deps.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(build_script_folder=os.path.join(self.source_folder, "simpleble"))
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["simpleble"]
+
